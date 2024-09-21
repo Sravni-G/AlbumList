bee0f61a177b484bfee2dd9bb55024bd92a2d5dd
Sravni-G <sravani.grandhisila19@gmail.com>
2024-09-21 19:50:22 +0530

    Routes changed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import List from "./components/List.js";
-import {
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Add from "./components/Add.js";
 import Delete from "./components/Delete.js";
 import Home from "./components/Home.js";
 import Update from "./components/Update.js";
 
 function App() {
-  const routes = createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/list" element={<List />}></Route>
-      <Route path="/add" element={<Add />}></Route>
-      <Route path="/delete" element={<Delete />}></Route>
-      <Route path="/update" element={<Update />}></Route>
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/list" element={<List />}></Route>
+        <Route path="/add" element={<Add />}></Route>
+        <Route path="/delete" element={<Delete />}></Route>
+        <Route path="/update" element={<Update />}></Route>
+      </Routes>
+    </Router>
   );
-
-  const router = createBrowserRouter(routes);
-  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
